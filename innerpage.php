e8606591ce69133aa4c598d1a7ad3cc152013976
Namita Ashodia <58926975+Namita-Ashodia@users.noreply.github.com>
2020-02-09 20:06:49 +0530

    Add files via upload

---
+++ innerpage.php
@@ -0,0 +1,158 @@
+<?php
+  
+  include 'navbar.php';
+
+?>
+
+  
+<div class="container-fluid">
+  <div class="row content">
+    <div class="col-sm-3 sidenav">
+      
+      <ul class="nav nav-pills nav-stacked">
+        <li class="active"><a href="#section1">Brand</a>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspPuma<br>
+        </li>
+        <li class="active"><a href="#section2">Price</a>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRs.199 to Rs.499<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRs.199 to Rs.499<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRs.199 to Rs.499<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRs.199 to Rs.499<br>
+        </li>
+        <li class="active"><a href="#section3">Colour</a>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRed<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRed<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRed<br>
+          &nbsp&nbsp<input type="checkbox" name="hhue">&nbspRed<br>
+        </li>
+        <li class="active"><a href="#section3">Discount</a>
+          &nbsp&nbsp<input type="radio" name="hhue">&nbspUpto 80%<br>
+          &nbsp&nbsp<input type="radio" name="hhue">&nbspUpto 80%<br>
+          &nbsp&nbsp<input type="radio" name="hhue">&nbspUpto 80%<br>
+          &nbsp&nbsp<input type="radio" name="hhue">&nbspUpto 80%<br>
+          &nbsp&nbsp<input type="radio" name="hhue">&nbspUpto 80%<br>
+        </li>
+      </ul><br>
+      
+    </div>
+
+<div class="col-sm-9">    
+  <div class="row">
+   
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Nike</div>
+        <div class="panel-body"><img src="img/nike.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-1.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+
+
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Puma</div>
+        <div class="panel-body"><img src="img/puma.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        
+        <div class="panel-footer">
+          <form action="inner-2.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+        
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Roadster</div>
+        <div class="panel-body"><img src="img/roadster.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-3.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Adidas</div>
+        <div class="panel-body"><img src="img/adidas.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-4.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Adidas Originals</div>
+        <div class="panel-body"><img src="img/adidas_originals.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-5.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Reebok</div>
+        <div class="panel-body"><img src="img/reebok.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-6.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+    
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Wrogn</div>
+        <div class="panel-body"><img src="img/wrogn.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-7.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">HRX</div>
+        <div class="panel-body"><img src="img/hrx.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-8.php">
+            <input type="submit" name="button" value="Buy"></div>
+        </form>
+      </div>
+    </div>
+
+    <div class="col-sm-4">
+      <div class="panel panel-primary">
+        <div class="panel-heading">Benetton</div>
+        <div class="panel-body"><img src="img/benetton.jpg" class="img-responsive" style="width:100%" alt="Image"></div>
+        <div class="panel-footer">
+          <form action="inner-9.php">
+          <input type="submit" name="button" value="Buy">
+        </form>
+        </div>
+
+      </div>
+    </div>
+
+</div><br>
+
+</div>
+</div>
+</div>
+
+<?php include 'footer.php'; ?>
+
